79b81625d5260aeeb86c5ac73a162bd8d9122979
Beau Bradley <beaubradley@outlook.com>
2020-10-09 11:31:21 -0400

    init comimit

---
+++ migrations/20201009111051_projects.js
@@ -0,0 +1,40 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('resources', table => {
+    table.increments('id').unsigned().primary();
+    table.string('name').notNull();
+    table.text('description').nullable();
+  })
+ 
+  .createTable('projects', table => {
+    table.increments('id').unsigned().primary();
+    table.string('name').notNull();
+    table.text('description').nullable();
+
+    table.integer('resource_id')
+        .unsigned()
+        .references('id')
+        .inTable('resources')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+  })
+  .createTable('tasks', table => {
+    table.increments('id').unsigned().primary();
+    table.text('description').notNull();
+    table.text('notes').nullable();
+
+    table.integer('project_id')
+    .unsigned()
+    .references('id')
+    .inTable('projects')
+    .onDelete('RESTRICT')
+    .onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources')
+};
